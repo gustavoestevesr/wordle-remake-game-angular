75d77d5fcbfb1e2f508c694b1991f16e5108b98e
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-26 11:21:34 -0300

    feat: estilização e lógica do teclado com desabilitar e dicas coloridas

--- src/app/components/keyboard-button/keyboard-button.component.ts
+++ src/app/components/keyboard-button/keyboard-button.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-keyboard-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './keyboard-button.component.html',
   styleUrl: './keyboard-button.component.scss'
 })
 export class KeyboardButtonComponent {
+  @Input() letter!: string;
+  @Input() disabled!: boolean;
+  @Input() tip!: 'correct' | 'almost';
+  @Output() buttonClicked = new EventEmitter<string>();
 
+  onButtonClick() {
+    if (!this.disabled) {
+      this.buttonClicked.emit(this.letter);
+    }
+  }
 }
--- src/app/components/keyboard/keyboard.component.ts
+++ src/app/components/keyboard/keyboard.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { KeyboardButtonComponent } from '../keyboard-button/keyboard-button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-keyboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, KeyboardButtonComponent],
   templateUrl: './keyboard.component.html',
   styleUrl: './keyboard.component.scss'
 })
 export class KeyboardComponent {
+  public readonly qwertyAlphabet = [
+    { label: 'Q', disabled: false },
+    { label: 'W', disabled: false },
+    { label: 'E', disabled: false },
+    { label: 'R', disabled: false },
+    { label: 'T', disabled: false },
+    { label: 'Y', disabled: false },
+    { label: 'U', disabled: false },
+    { label: 'I', disabled: false },
+    { label: 'O', disabled: false },
+    { label: 'P', disabled: false },
+    { label: 'A', disabled: false },
+    { label: 'S', disabled: false },
+    { label: 'D', disabled: false },
+    { label: 'F', disabled: false },
+    { label: 'G', disabled: false },
+    { label: 'H', disabled: false },
+    { label: 'J', disabled: false },
+    { label: 'K', disabled: false },
+    { label: 'L', disabled: false },
+    { label: 'Z', disabled: false },
+    { label: 'X', disabled: false },
+    { label: 'C', disabled: false },
+    { label: 'DEL', disabled: false },
+    { label: 'V', disabled: false },
+    { label: 'B', disabled: false },
+    { label: 'N', disabled: false },
+    { label: 'M', disabled: false },
+    { label: 'ENTER', disabled: false }
+  ];
 
+  buttonClicked(letterButton: string) {
+    // Return nothing if it was pressed ENTER OR DELETE
+    if (letterButton === 'ENTER' || letterButton === 'DEL') return;
+
+    // Find the letter in qwertyAlphabet
+    const letterIndex = this.qwertyAlphabet.findIndex((letter) => letter.label === letterButton)
+
+    // Return nothing if it doesn't find
+    if (letterIndex === -1) return;
+
+    // Change the state of disabled
+    this.qwertyAlphabet[letterIndex].disabled = true;
+  }
 }
