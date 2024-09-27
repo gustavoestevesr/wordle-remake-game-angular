4eb7b5dcd45d8d6339909612a1131a486a4beb6d
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-27 15:40:02 -0300

    feat: responsividade do teclado

--- src/app/components/keyboard-button/keyboard-button.component.ts
+++ src/app/components/keyboard-button/keyboard-button.component.ts
@@ -1,22 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-keyboard-button',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './keyboard-button.component.html',
-  styleUrl: './keyboard-button.component.scss'
+  styleUrl: './keyboard-button.component.scss',
 })
 export class KeyboardButtonComponent {
   @Input() letter!: string;
-  @Input() disabled!: boolean;
   @Input() tip!: 'correct' | 'almost' | 'none';
   @Output() letterClicked = new EventEmitter<string>();
 
   onLetterClicked() {
-    if (!this.disabled) {
-      this.letterClicked.emit(this.letter);
-    }
+    this.letterClicked.emit(this.letter);
   }
+
+  // @HostListener('window:keydown', ['$event'])
+  //  handleKeyboardEvent(event: KeyboardEvent) {
+  //   console.log(event.key);
+  // }
 }
--- src/app/components/keyboard/keyboard.component.ts
+++ src/app/components/keyboard/keyboard.component.ts
@@ -6,7 +6,6 @@ import { WordleService } from '../../services/wordle.service';
 export interface Slot {
   key: string;
   tip: string;
-  disabled: boolean;
 }
 
 @Component({
@@ -20,62 +19,54 @@ export class KeyboardComponent {
   private wordleService = inject(WordleService);
 
   public readonly qwertyAlphabet1: Slot[] = [
-    { key: 'Q', tip: 'none', disabled: false },
-    { key: 'W', tip: 'none', disabled: false },
-    { key: 'E', tip: 'none', disabled: false },
-    { key: 'R', tip: 'none', disabled: false },
-    { key: 'T', tip: 'none', disabled: false },
-    { key: 'Y', tip: 'none', disabled: false },
-    { key: 'U', tip: 'none', disabled: false },
-    { key: 'I', tip: 'none', disabled: false },
-    { key: 'O', tip: 'none', disabled: false },
-    { key: 'P', tip: 'none', disabled: false },
+    { key: 'Q', tip: 'none' },
+    { key: 'W', tip: 'none' },
+    { key: 'E', tip: 'none' },
+    { key: 'R', tip: 'none' },
+    { key: 'T', tip: 'none' },
+    { key: 'Y', tip: 'none' },
+    { key: 'U', tip: 'none' },
+    { key: 'I', tip: 'none' },
+    { key: 'O', tip: 'none' },
+    { key: 'P', tip: 'none' },
   ];
 
   public readonly qwertyAlphabet2: Slot[] = [
-    { key: 'A', tip: 'none', disabled: false },
-    { key: 'S', tip: 'none', disabled: false },
-    { key: 'D', tip: 'none', disabled: false },
-    { key: 'F', tip: 'none', disabled: false },
-    { key: 'G', tip: 'none', disabled: false },
-    { key: 'H', tip: 'none', disabled: false },
-    { key: 'J', tip: 'none', disabled: false },
-    { key: 'K', tip: 'none', disabled: false },
-    { key: 'L', tip: 'none', disabled: false },
+    { key: 'A', tip: 'none' },
+    { key: 'S', tip: 'none' },
+    { key: 'D', tip: 'none' },
+    { key: 'F', tip: 'none' },
+    { key: 'G', tip: 'none' },
+    { key: 'H', tip: 'none' },
+    { key: 'J', tip: 'none' },
+    { key: 'K', tip: 'none' },
+    { key: 'L', tip: 'none' },
   ];
 
   public readonly qwertyAlphabet3: Slot[] = [
-    { key: 'DEL', tip: 'none', disabled: false },
-    { key: 'Z', tip: 'none', disabled: false },
-    { key: 'X', tip: 'none', disabled: false },
-    { key: 'C', tip: 'none', disabled: false },
-    { key: 'V', tip: 'none', disabled: false },
-    { key: 'B', tip: 'none', disabled: false },
-    { key: 'N', tip: 'none', disabled: false },
-    { key: 'M', tip: 'none', disabled: false },
-    { key: 'ENTER', tip: 'none', disabled: false },
+    { key: '⌫', tip: 'none' },
+    { key: 'Z', tip: 'none' },
+    { key: 'X', tip: 'none' },
+    { key: 'C', tip: 'none' },
+    { key: 'V', tip: 'none' },
+    { key: 'B', tip: 'none' },
+    { key: 'N', tip: 'none' },
+    { key: 'M', tip: 'none' },
+    { key: '⏎', tip: 'none' },
   ];
 
   getLetterClicked(letterSelected: string) {
-    if (letterSelected === 'DEL') {
+    if (letterSelected === '⌫') {
       this.wordleService.removeLastLetterFromMatrix();
       return;
     }
 
-    if (letterSelected === 'ENTER') {
+    if (letterSelected === '⏎') {
       this.wordleService.submitWordToValidation();
       return;
     }
 
     // Change the state of letter
     this.wordleService.addLetterIntoMatrix(letterSelected);
-  }
-
-  private disableLetterButton() {
-    // const letterIndex = this.qwertyAlphabet.findIndex(
-    //   (letter) => letter.key === letterSelected
-    // );
-    // if (letterIndex === -1) return;
-    // this.qwertyAlphabet[letterIndex].disabled = true;
   }
 }
