78ed5543cd5e8342e26efeae358137145effa512
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-27 11:39:29 -0300

    feat: renderização da matriz de slots e estilização de cores com animação

--- src/app/components/slots-array/slots-array.component.ts
+++ src/app/components/slots-array/slots-array.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { WordleService } from '../../services/wordle.service';
 
 @Component({
   selector: 'app-slots-array',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './slots-array.component.html',
   styleUrl: './slots-array.component.scss'
 })
 export class SlotsArrayComponent {
-
+  public wordleService = inject(WordleService)
 }
