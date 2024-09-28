7061a20dfa787691981609afeb4141b7044b0e66
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-27 21:18:28 -0300

    feat: implementando modal de alerta com dados dinâmicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component, inject } from '@angular/core';
-import { KeyboardComponent } from './components/keyboard/keyboard.component';
-import { WordleService } from './services/wordle.service';
-import { SlotsArrayComponent } from './components/slots-array/slots-array.component';
 import { HeaderComponent } from './components/header/header.component';
+import { KeyboardComponent } from './components/keyboard/keyboard.component';
+import { SlotsArrayComponent } from './components/slots-array/slots-array.component';
+import { AlertModalComponent } from './modals/alert-modal/alert-modal.component';
+import { WordleService } from './services/wordle.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, SlotsArrayComponent, KeyboardComponent],
+  imports: [HeaderComponent, SlotsArrayComponent, KeyboardComponent, AlertModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  public wordleService = inject(WordleService)
 }
